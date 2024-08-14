2e6b42337f792d77b45722994908ac089d59c4ed
ics-ajay <ajay.bisht@inaraconsultancy.com>
2024-08-14 18:41:16 +0530

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-900 text-white p-6">
+        <h1 className="text-4xl font-bold">
+          Welcome to React with Tailwind CSS
+        </h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
